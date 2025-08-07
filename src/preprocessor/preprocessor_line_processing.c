5294c944b9343073a0c838dd37750bfbb7d9fc1d
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-07 14:57:44 +0300

    Improve octal parsing and update test resources
    Enhanced octal number parsing in the preprocessor to detect invalid octal literals containing digits 8 or 9 and report an error. Updated the standard version macro value to 10150L. Expanded test resource files and test logic to support multiple resource paths and verify macro expansion and include handling.

--- src/preprocessor/preprocessor_line_processing.c
+++ src/preprocessor/preprocessor_line_processing.c
@@ -103,7 +103,7 @@ bool scan_and_expand_macros_for_expressions(
             }
             else if (wcscmp(identifier, L"__إصدار_المعيار_باء__") == 0)
             {
-                if (!append_to_dynamic_buffer(one_pass_buffer, L"10010L"))
+                if (!append_to_dynamic_buffer(one_pass_buffer, L"10150L"))
                 {
                     PpSourceLocation temp_loc = get_current_original_location(pp_state);
                     temp_loc.line = original_line_number_for_errors;
@@ -750,7 +750,7 @@ bool scan_and_substitute_macros_one_pass(
             }
             else if (wcscmp(identifier, L"__إصدار_المعيار_باء__") == 0)
             {
-                if (!append_to_dynamic_buffer(one_pass_buffer, L"10010L"))
+                if (!append_to_dynamic_buffer(one_pass_buffer, L"10150L"))
                 {
                     PpSourceLocation temp_loc = get_current_original_location(pp_state);
                     temp_loc.line = original_line_number_for_errors;
